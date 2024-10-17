acde4402594b6ea6b68759c50dfdc99480ed813b
Maxi1972Tara <166968334+Maxi1972Tara@users.noreply.github.com>
2024-10-17 17:56:27 -0300

    Actualización con los errores pedidos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,56 @@ const routes: Routes = [
   },
   {
     path: 'login',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'recover-password',
+    redirectTo: 'recover-password',
+    pathMatch: 'full'
+  },
+  {
+    path: 'principal',
+    redirectTo: 'principal',
+    pathMatch: 'full'
+  },
+  {
+    path: 'creacioncuenta',
+    redirectTo: 'creacioncuenta',
+    pathMatch: 'full'
+  },
+  {
+    path: 'creacioncuenta',
+    redirectTo: 'creacioncuenta',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'e404',
+    pathMatch: 'full'
+  },
+  {
+    path: 'e500',
+    redirectTo: 'e500',
+    pathMatch: 'full'
+  },
+  {
+    path: 'e502',
+    redirectTo: 'e502',
+    pathMatch: 'full'
+  },
+  {
+    path: 'recuperar-tne',
+    redirectTo: 'recuperar-tne',
+    pathMatch: 'full'
+  },
+  {
+    path: 'revalidar-tne',
+    redirectTo: 'revalidar-tne',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
   {
@@ -24,13 +74,26 @@ const routes: Routes = [
     loadChildren: () => import('./pages/creacioncuenta/creacioncuenta.module').then( m => m.CreacioncuentaPageModule)
   },
   {
+    path: 'e404',
+    loadChildren: () => import('./pages/e404/e404.module').then( m => m.E404PageModule)
+  },
+  {
+    path: 'e500',
+    loadChildren: () => import('./pages/e500/e500.module').then( m => m.E500PageModule)
+  },
+  {
+    path: 'e502',
+    loadChildren: () => import('./pages/e502/e502.module').then( m => m.E502PageModule)
+  },
+  {
     path: 'recuperar-tne',
-    loadChildren: () => import('./recuperar-tne/recuperar-tne.module').then( m => m.RecuperarTnePageModule)
+    loadChildren: () => import('./pages/recuperar-tne/recuperar-tne.module').then( m => m.RecuperarTnePageModule)
   },
   {
     path: 'revalidar-tne',
-    loadChildren: () => import('./revalidar-tne/revalidar-tne.module').then( m => m.RevalidarTnePageModule)
+    loadChildren: () => import('./pages/revalidar-tne/revalidar-tne.module').then( m => m.RevalidarTnePageModule)
   },
+
 
 ];
 
